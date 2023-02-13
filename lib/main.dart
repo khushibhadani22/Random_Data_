b6c654afb4ca5270366621c70bfdac41d41ad054
Khushi bhadani <khushibhadani22410@gmail.com>
2023-02-13 19:39:57 +0530

    Last

--- lib/main.dart
+++ lib/main.dart
@@ -55,85 +55,138 @@ class _MyAppState extends State<MyApp> {
           } else if (snapshot.hasData) {
             RanDom? data = snapshot.data;
             return (data != null)
-                ? Column(
-                    children: [
-                      Image.network(data.image),
-                      Text(
-                        "AGE :- ${data.age}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
+                ? Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      height: 500,
+                      width: 450,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 3)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "NAME :-\n${data.title} ${data.first} ${data.last}",
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
+                                    const SizedBox(
+                                      height: 7,
+                                    ),
+                                    Text(
+                                      "GENDER :- ${data.gender}",
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
+                                    const SizedBox(
+                                      height: 7,
+                                    ),
+                                    Text(
+                                      "AGE :- ${data.age}",
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
+                                  ],
+                                ),
+                                const Spacer(),
+                                CircleAvatar(
+                                    radius: 70,
+                                    backgroundImage: NetworkImage(data.image)),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            Text(
+                              "DOB :- ${data.dobDate}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "EMAIL :- ${data.email}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "CITY :- ${data.city}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "STATE :- ${data.state}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "COUNTRY :- ${data.country}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "PHONE :- ${data.phone}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "CELL :- ${data.cell}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "REGISTER DATE :- ${data.registerDate}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "STREET NUMBER :- ${data.streetNumber}",
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            const Spacer(),
+                          ],
+                        ),
                       ),
-                      Text(
-                        "GENDER :- ${data.gender}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "TITLE :- ${data.title}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "FIRST :- ${data.first}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "LAST :- ${data.last}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "STREET NUMBER :- ${data.streetNumber}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "EMAIL :- ${data.email}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "CITY :- ${data.city}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "STATE :- ${data.state}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "COUNTRY :- ${data.country}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "PHONE :- ${data.phone}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "CELL :- ${data.cell}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "DOB :- ${data.dobDate}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "REGISTER DATE :- ${data.registerDate}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "AGE :- ${data.age}",
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                    ),
                   )
                 : const Text(
                     "No Data...",
--- lib/model.dart
+++ lib/model.dart
@@ -4,56 +4,53 @@ class RanDom {
   final String first;
   final String last;
   final String image;
-
-  final int streetNumber;
-  final int age;
-
   final String city;
   final String state;
   final String country;
   final String email;
   final String dobDate;
   final String registerDate;
-//  final String registerAge;
   final String phone;
   final String cell;
+
+  final int streetNumber;
+  final int age;
 
   RanDom({
     required this.gender,
     required this.title,
     required this.first,
     required this.last,
-    required this.streetNumber,
     required this.city,
     required this.state,
     required this.country,
     required this.email,
     required this.dobDate,
-    required this.age,
     required this.image,
     required this.registerDate,
-    //  required this.registerAge,
     required this.phone,
     required this.cell,
+    required this.streetNumber,
+    required this.age,
   });
 
   factory RanDom.fromJson({required Map json}) {
     return RanDom(
-        gender: json['results'][0]['gender'],
-        title: json['results'][0]['name']['title'],
-        first: json['results'][0]['name']['first'],
-        last: json['results'][0]['name']['last'],
-        streetNumber: json['results'][0]['location']['street']['number'],
-        city: json['results'][0]['location']['city'],
-        state: json['results'][0]['location']['state'],
-        country: json['results'][0]['location']['country'],
-        email: json['results'][0]['email'],
-        dobDate: json['results'][0]['dob']['date'],
-        age: json['results'][0]['dob']['age'],
-        registerDate: json['results'][0]['registered']['date'],
-        // registerAge: json['results'][0]['registered']['age'],
-        phone: json['results'][0]['phone'],
-        image: json['results'][0]['picture']['large'],
-        cell: json['results'][0]['cell']);
+      gender: json['results'][0]['gender'],
+      title: json['results'][0]['name']['title'],
+      first: json['results'][0]['name']['first'],
+      last: json['results'][0]['name']['last'],
+      city: json['results'][0]['location']['city'],
+      state: json['results'][0]['location']['state'],
+      country: json['results'][0]['location']['country'],
+      email: json['results'][0]['email'],
+      dobDate: json['results'][0]['dob']['date'],
+      registerDate: json['results'][0]['registered']['date'],
+      phone: json['results'][0]['phone'],
+      image: json['results'][0]['picture']['large'],
+      cell: json['results'][0]['cell'],
+      streetNumber: json['results'][0]['location']['street']['number'],
+      age: json['results'][0]['dob']['age'],
+    );
   }
 }
